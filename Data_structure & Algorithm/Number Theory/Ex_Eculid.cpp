4000c89687ebeaf396421800c13d897b54856b79
Joy Mohajon <92334273+mohajon33@users.noreply.github.com>
2023-01-21 02:11:23 +0600

    added Extended Euclidean Algorithm code

---
+++ Data_structure & Algorithm/Number Theory/Ex_Eculid.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+ll ex_gcd(ll a, ll b, ll& x, ll& y)
+{
+    if(b==0){
+        x = 1;
+        y = 0;
+        return a;
+    }
+    ll x1, y1;
+    ll gcd = ex_gcd(b, a%b, x1, y1);
+    x = y1;
+    y = x1 - y1*(a/b);
+    return gcd;
+}
+
+int main()
+{
+    ll a, b, x, y;
+
+    while(cin >> a >> b){
+        ll x, y;
+        ll gcd = a > b ?ex_gcd(a, b, x, y):ex_gcd(b, a, x, y);
+
+        if(a >= b)
+           cout << x << " " << y << " " << gcd << endl;
+        else 
+           cout << y << " " << x << " " << gcd << endl; 
+    }
+    return 0;
+}
